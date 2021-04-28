479b7eb042038a6bfba8f79944cdcd14856b65c5
GaozyDev <670453367@qq.com>
2021-04-28 18:07:30 +0800

    接入Eureka

--- server/src/main/java/com/gl/order/OrderApplication.java
+++ server/src/main/java/com/gl/order/OrderApplication.java
@@ -2,8 +2,12 @@ package com.gl.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients(basePackages = "com.gl.product.client")
 public class OrderApplication {
 
     public static void main(String[] args) {
