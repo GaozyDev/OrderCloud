540150a7473cba02c5e9a54c5f0a25b6a2929be2
GaozyDev <670453367@qq.com>
2021-05-09 01:17:48 +0800

    rabbitmq

---
+++ server/src/main/java/com/gl/order/config/GirlConfig.java
@@ -0,0 +1,17 @@
+package com.gl.order.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "girl")
+@RefreshScope
+public class GirlConfig {
+
+	private String name;
+
+	private Integer age;
+}
